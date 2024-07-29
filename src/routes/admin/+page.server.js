0dd3e0cd5c95647b9db787c308a478256edf4e41
khmerweb <vuthdevelop1@gmail.com>
2024-07-30 02:08:36 +0700

    Make it better

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit'
 export function load({ locals }) {
 	const user = locals.user
     if(!user){
-        throw redirect(307, '/login')
+        throw redirect(307, '/login')//
     }
 
     return {user: user}
