a4c5564cb7bb2eb192faa4ba2801128b9091c100
khmerweb <vuthdevelop1@gmail.com>
2025-05-30 16:31:29 +0700

    Make it better

--- src/routes/(app)/+page.server.js
+++ src/routes/(app)/+page.server.js
@@ -3,7 +3,7 @@ import postDb from '$lib/db/post.js'
 export async function load({ locals, fetch }) {
     const count = await postDb.count(locals)
     const settings = await locals.settings(locals)
-    const posts = await postDb.getPosts(locals, settings.indexPostLimit)
+    //const posts = await postDb.getPosts(locals, settings.indexPostLimit)
     const latestPosts = await postDb.getLatestPosts(locals, 20)
     const categories = ['movie', 'travel', 'doc', 'sport', 'game', 'food', 'music', 'distraction']
     const postsByCategory = await postDb.getLatestPostByCategory(locals, categories, 20)
@@ -26,7 +26,7 @@ export async function load({ locals, fetch }) {
             }
         }
 
-        return {posts, count, settings, latestPosts, postsByCategory, thumbs, pageURL, title}
+        return {count, settings, latestPosts, postsByCategory, thumbs, pageURL, title}
     }
     
     const pageURL = '/'
