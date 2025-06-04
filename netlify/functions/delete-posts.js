557e0fe3b44c2d3ddd3f96ff24074b6d93668edd
khmerweb <vuthdevelop1@gmail.com>
2025-06-05 00:48:34 +0700

    Make it better

--- netlify/functions/delete-posts.js
+++ netlify/functions/delete-posts.js
@@ -3,7 +3,7 @@ import postDb from "../../src/lib/db/post.js"
 
 export default async (req) => {
     const { next_run } = await req.json()
-    //postDb.deletePosts(prisma)
+    postDb.deletePosts(prisma)
     //fetch("/.netlify/functions/delete-posts-background", { method: "POST" })
     console.log("Received event! Next invocation at:", next_run)
 }
