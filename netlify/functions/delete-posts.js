bea1349405906d09e47bd6c21ce4492c2efc00c9
khmerweb <vuthdevelop1@gmail.com>
2025-06-05 00:28:00 +0700

    Make it better

--- netlify/functions/delete-posts.js
+++ netlify/functions/delete-posts.js
@@ -2,7 +2,7 @@
 export default async (req) => {
     const { next_run } = await req.json()
 
-    fetch("/.netlify/functions/delete-posts-background", { method: "POST" })
+    //fetch("/.netlify/functions/delete-posts-background", { method: "POST" })
     console.log("Received event! Next invocation at:", next_run)
 }
 //daily
