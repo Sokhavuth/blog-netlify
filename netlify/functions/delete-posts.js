8f4dce9780e7caa7cb6cf64e3729fd8b817da297
khmerweb <vuthdevelop1@gmail.com>
2025-06-05 10:43:58 +0700

    Make it better

--- netlify/functions/delete-posts.js
+++ netlify/functions/delete-posts.js
@@ -1,10 +1,22 @@
-//import prisma from "../../src/lib/db/prisma.js"
-//import postDb from "../../src/lib/db/post.js"
+import prisma from "../../src/lib/db/prisma.js"
+import postDb from "../../src/lib/db/post.js"
 
 export default async (req) => {
     const { next_run } = await req.json()
-    //postDb.deletePosts(prisma)
-    fetch("https://khmerweb-live.netlify.app/.netlify/functions/delete-posts-background", { method: "POST" })
+    let request = {}
+    let thumbs = []
+    request.prisma = prisma
+    const posts = await postDb.getPosts(request, "all")
+    for(let item of posts){
+        const url = item.thumb
+        const res = await fetch(url)
+        if(res.status !== 200){
+            thumbs.push(url)
+        }
+    }
+
+    postDb.deletePosts(request, thumbs)
+    //fetch("https://khmerweb-live.netlify.app/.netlify/functions/delete-posts-background", { method: "POST" })
     console.log("Received event! Next invocation at:", next_run)
 }
 //daily
